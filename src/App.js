cd57bedce0089a7f00f46e169766b71f97f0e0c7
lamroj02 <lamroj02@gmail.com>
2024-09-30 15:13:12 +0100

    Tracked jobs are now displayed in a table; Tracked jobs are now stored locally, meaning they persist between sessions;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import JobForm from './components/Jobform/JobForm.js';
 import './App.css';
 
+const isNull = (storedItem) => {
+  return localStorage.getItem(storedItem) === null;
+};
+
 function App() {
-  const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState(
+    isNull("jobs") ? [] : JSON.parse(localStorage.getItem("jobs"))
+  );
+
+  useEffect(() => {
+    localStorage.setItem("jobs", JSON.stringify(jobs));
+  }, [jobs]);
 
   const addJob = (newJob) => {
-    setJobs([...jobs, newJob]);
+    setJobs((prevJobs) => {
+      const updatedJobs = [...prevJobs, newJob];
+      return updatedJobs;
+    });
   };
 
   return (
@@ -14,13 +27,27 @@ function App() {
       <h1>Job Application Tracker</h1>
       <h3>by Lamroj02</h3>
       <JobForm addJob={addJob} />
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>
-            {job.company} - {job.position} ({job.status}) - Applied on {job.dateApplied}
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Position</th>
+            <th>Status</th>
+            <th>Date Applied</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jobs.map((job, index) => (
+            <tr key={index}>
+              <td>{job.company}</td>
+              <td>{job.position}</td>
+              <td>{job.status}</td>
+              <td>{job.dateApplied}</td>
+              <td><button type="button">Edit</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
