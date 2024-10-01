56756b352b4057dfd9ac9042a1a07e83f499df31
lamroj02 <lamroj02@gmail.com>
2024-10-01 18:36:56 +0100

    Made status editable and jobs deletable;

--- src/App.js
+++ src/App.js
@@ -15,10 +15,42 @@ function App() {
     localStorage.setItem("jobs", JSON.stringify(jobs));
   }, [jobs]);
 
+  //CRUD Operations
+
   const addJob = (newJob) => {
     setJobs((prevJobs) => {
       const updatedJobs = [...prevJobs, newJob];
       return updatedJobs;
+    });
+  };
+
+  const sortJobs = (sortBy) => {
+    let sortedJobs = [];
+    if(sortBy==='company'){
+
+    }
+    else if(sortBy==='position'){
+
+    }
+    else if(sortBy==='status'){
+
+    }
+    else{
+      sortedJobs = [...jobs].sort((a, b) => a.company.localeCompare(b.company));
+    } 
+    setJobs(sortedJobs);
+  };
+
+  const remJob = (delJob) => {
+    setJobs((prevJobs) => {
+      return prevJobs.filter(_jobs => _jobs !== delJob)
+    });
+  };
+
+  const updateJob = (delJob, newJob) => {
+    setJobs((prevJobs) => {
+      prevJobs = prevJobs.filter(_jobs => _jobs !== delJob);
+      return [...prevJobs, newJob];
     });
   };
 
@@ -34,6 +66,8 @@ function App() {
             <th>Position</th>
             <th>Status</th>
             <th>Date Applied</th>
+            <th className='modTh'></th>
+            <th className='modTh'></th>
           </tr>
         </thead>
         <tbody>
@@ -41,9 +75,25 @@ function App() {
             <tr key={index}>
               <td>{job.company}</td>
               <td>{job.position}</td>
-              <td>{job.status}</td>
+              <td>
+                <select value={job.status} onChange={(e) => updateJob(job,{
+                  company: job.company,
+                  position: job.position,
+                  status: e.target.value,
+                  dateApplied: job.dateApplied,
+                })}>
+                  <option value="Applied">Applied</option>
+                  <option value="Interview">Interview</option>
+                  <option value="Offer">Offer</option>
+                  <option value="Rejected">Rejected</option>
+                </select>
+              </td>
               <td>{job.dateApplied}</td>
-              <td><button type="button">Edit</button></td>
+              <td><button onClick={() => {
+                    if (window.confirm("Are you sure you want to delete this job?")) {
+                      remJob(jobs[index]);
+                    }
+                  }}>Delete</button></td>
             </tr>
           ))}
         </tbody>
